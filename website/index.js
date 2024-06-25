a61e37c09f8fc8ecb226e4348e9a218f9f5abd1e
Abila <41573378+abila98@users.noreply.github.com>
2024-06-26 03:04:37 +0530

    Update index.js

--- website/index.js
+++ website/index.js
@@ -2,7 +2,7 @@ const counter = document.querySelector(".counter-number");
 
 async function updateCounter() {
     console.log("Updating counter...");
-    let response = await fetch("https://k4inz4zl2zsn4xjrwougjmp45u0dsohd.lambda-url.us-west-1.on.aws/");
+    let response = await fetch("https://jul7qeeflj4ecbjptkrxfefon40qfiob.lambda-url.us-west-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = `<p>This page has ${data} Views!</p>`;
 }
